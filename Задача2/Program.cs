c583a7126c832aeb0eff69b380896cf2390305e9
Your Pavel <p9l-real-chel@mail.ru>
2023-03-10 17:41:08 +0500

    исправление

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -2,7 +2,7 @@
 
 try
 {
-    Console.WriteLine("Введите два числа");
+    Console.WriteLine("Введите два целлых числа");
     int num1 = Convert.ToInt32(Console.ReadLine());
     int num2 = Convert.ToInt32(Console.ReadLine());
     if (num1 > num2) Console.WriteLine("Первое число больше чем второе");
