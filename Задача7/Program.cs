8a9a869d128c4a7df666a44fb8dc7b7ff4e70e78
Your Pavel <p9l-real-chel@mail.ru>
2023-03-10 18:21:38 +0500

    Задача7

---
+++ Задача7/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает третью цифру слева этого числа или говорит, что такой цифры нет.
+// Через строку решать нельзя.
+try
+{
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    num = Math.Abs(num);
+
+    if (num >= 100)
+    {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        int num1 = num % 10;
+        Console.WriteLine(num1);
+    }
+    else
+    {
+        Console.Write("Введенное число меньше 100");
+    }
+}
+catch
+{
+    Console.WriteLine("Введите число");
+}
