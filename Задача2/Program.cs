2261cebba0e05ca8fb264fdbe83c0e2fd0d010f4
Your Pavel <p9l-real-chel@mail.ru>
2023-03-10 16:52:26 +0500

    задача 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{
+    Console.WriteLine("Введите два числа");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    if (num1 > num2) Console.WriteLine("Первое число больше чем второе");
+    else Console.WriteLine("Второе число больше чем первое");
+}
+catch
+{
+    Console.WriteLine("Введите числа");
+}
